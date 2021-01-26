5e0e7ce699e3807502be52ea1036a31ea5c1f6df
jcheron <myaddressmail@gmail.com>
2021-01-26 02:56:33 +0100

    add service

--- app/controllers/TodosController.php
+++ app/controllers/TodosController.php
@@ -2,175 +2,116 @@
 
 namespace controllers;
 
+use services\SessionStorageList;
 use Ubiquity\attributes\items\router\Post;
 use Ubiquity\attributes\items\router\Get;
 use Ubiquity\attributes\items\router\Route;
-use Ubiquity\cache\CacheManager;
-use Ubiquity\controllers\Router;
 use Ubiquity\utils\http\URequest;
-use Ubiquity\utils\http\USession;
 
 /**
  * Controller TodosController
  * @property \Ajax\php\ubiquity\JsUtils $jquery
  */
 class TodosController extends ControllerBase {
-	const KEY = 'datas/lists/';
 
-	private $info;
+	private SessionStorageList $service;
 
 	private function showMessage(string $header, string $message, string $type = '', string $icon = 'info circle',array $buttons=[]) {
 		$this->loadView('main/vMessage.html', compact('header', 'type', 'icon', 'message','buttons'));
 	}
 
-	private function displayList(array $list, ?int $toEdit = null) {
+	private function displayList(array $list) {
 		if(\count($list)>0){
 			$this->jquery->show('._saveList','','',true);
 		}
-		$title=$this->info['id'];
-		if($this->info['updated']){
-			$title.=' [*]';
-		}
+		$title=$this->service->getTitle();
 		$js = 'let $item=$(event.target).closest("div.item");$item.children(".ui.checkbox").toggle();$item.children("form").toggle();';
 		$this->jquery->click('._toEdit', $js,true,true);
 		$this->jquery->change('#multiple','$("._form").toggle();',false,false,false);
 		$this->jquery->dblclick('._element', $js,true,true);
-		//$this->jquery->getOn('dblclick','._element',Router::path('todos.toEdit'),'body .ui.container',['attr'=>'data-index','hasLoader'=>false]);
-		$this->jquery->renderView('TodosController/displayList.html', compact('list', 'toEdit','title'));
-	}
-
-	private function getList(): array {
-		return USession::get('list', []);
-	}
-
-	private function setList(array $list): void {
-		$this->listUpdated(true);
-		USession::set('list', $list);;
-	}
-
-	private function listUpdated(bool $updated=false):void{
-		$info=USession::get('active-list',['id'=>'not saved']);
-		$this->setInfo($info['id'],$updated);
-	}
-
-	private function setInfo(string $id=null,bool $updated=false){
-		$this->info=['id'=>$id??'not saved','updated'=>$updated];
-		if(isset($id)) {
-			USession::set('active-list', $this->info);
-		}
-	}
-
-	public function getUniqid(){
-		return ($this->info['id']==='not saved')?uniqid('',true):$this->info['id'];
-	}
-
-	private function getInfo(){
-		$this->info=USession::get('active-list',['id'=>'not saved']);
+		$this->jquery->getHref('a','.ui.container',['hasLoader'=>false,'historize'=>false]);
+		$this->jquery->renderView('TodosController/displayList.html', compact('list','title'));
 	}
 
 	public function initialize() {
 		parent::initialize();
+		$this->service=new SessionStorageList($this);
 		$this->loadView("TodosController/menu.html");
 	}
 
-	private function createCopyButton(string $bt,string $elmText){
-		$this->jquery->click($bt,'let $temp = $("<input>");$("body").append($temp);$temp.val($("'.$elmText.'").text()).select();document.execCommand("copy");$temp.remove();$("'.$bt.'").popup({content: "copié!"}).popup("show");');
-	}
 
-	private function getButtonUrl($url){
-		$this->createCopyButton('#btCopy','#url');
-		return '<span id="url" class="ui inverted label">'.$url.'</span>&nbsp;<div class="ui mini circular inverted basic icon button" id="btCopy"><i class="copy icon"></i></div>';
-	}
 
 	#[Route('_default', name: 'home')]
 	public function index() {
-		if (USession::exists('list')) {
-			$this->getInfo();
-			return $this->displayList(USession::get('list'));
+		if ($this->service->listExist()) {
+			return $this->displayList($this->service->getList(false));
 		}
 		$this->jquery->renderView("TodosController/index.html");
 	}
 
 	#[Get(path: "todos/new/{force}", name: 'todos.new')]
 	public function newlist(?bool $force=false) {
-		if($force===false && USession::exists('list')){
+		if($force===false && $this->service->listExist()){
 			return $this->showMessage('Nouvelle liste','Une liste a déjà été créée. Souhaitez-vous la vider ?','warning','warning circle alternate',[
 				['url'=>['Home',[]],'caption'=>'Annuler','class'=>''],
 				['url'=>['todos.new',[true]],'caption'=>'Confirmer la création','class'=>'green']
 			]);
 		}
-		$list = USession::set('list', []);
-		$this->info=['id'=>'not saved','updated'=>false];
-		USession::set('active-list',$this->info);
 		$this->showMessage('Nouvelle Liste', "Liste correctement créée.", 'success', 'check square outline');
-		$this->displayList($list);
+		$this->displayList($this->service->createList());
 	}
 
 
 	#[Post(path: "todos/add", name: 'todos.add')]
 	public function addElement() {
-		$elm = URequest::post('element');
+		$toAdd = URequest::post('element');
 		$elms = URequest::post('elements');
-		$list = $this->getList();
-		if ($elms) {
-			$elems=explode("\n",$elms);
-			foreach ($elems as $e) {
-				if($e!='') {
-					$list[] = $e;
-				}
-			}
-		}elseif ($elm){
-			$list[] = $elm;
+		if($elms){
+			$toAdd=explode("\n",$elms);
 		}
-		$this->setList($list);
-		$this->displayList($list);
+		if(isset($toAdd)){
+			$list=$this->service->addElements($toAdd);
+			return $this->displayList($list);
+		}
+		$this->displayList($this->service->getList());
 	}
 
 
 	#[Get(path: "todos/delete/{index}", name: 'todos.delete')]
 	public function deleteElement(int $index) {
-		$list = $this->getList();
-		if (isset($list[$index])) {
-			$this->showMessage('Suppression', "L'élément <b>$list[$index]</b> a été supprimé.", 'error', 'check square outline');
-			unset($list[$index]);
-			$list=\array_values($list);
-			$this->setList($list);
+		$list=$this->service->deleteElement($index);
+		if (!$this->service->hasError()) {
+			$this->showMessage('Suppression', $this->service->getMessage('success'), 'success', 'check square outline');
+			return $this->displayList($list);
 		}
-		$this->displayList($list);
 	}
 
 
 	#[Post(path: "todos/edit/{index}", name: 'todos.edit')]
 	public function edit($index) {
-		$list = $this->getList();
-		if (isset($list[$index])) {
-			$elm = URequest::post('element');
-			if ($elm) {
-				$list[$index] = $elm;
-				$this->setList($list);
-			}
+		$list=$this->service->updateElement($index,URequest::post('element'));
+		if (!$this->service->hasError()) {
+			$this->showMessage('Modification', $this->service->getMessage('success'), 'success', 'check square outline');
+			return $this->displayList($list);
 		}
-		$this->displayList($list);
 	}
 
 
 	#[Get('todos/saveList', name: 'todos.save')]
 	public function saveList() {
-		$this->getInfo();
-		$id = $this->getUniqid();
-		$this->setInfo($id);
-		CacheManager::$cache->store(self::KEY . $id, $this->getList());
-		$this->showMessage('Sauvegarde', "La liste a été sauvegardée sous l'id <b>$id</b> et sera accessible depuis l'url " . $this->getButtonUrl(Router::url('todos.loadList', [$id])), 'success', 'check square outline');
+		$id=$this->service->persistentSave();
+		if($this->service->hasError()){
+			$this->showMessage('Sauvegarde', $this->service->getMessage('error'), 'error', 'warning circle');
+		}else {
+			$this->showMessage('Sauvegarde', $this->service->getMessage('success'), 'success', 'check square outline');
+		}
 		$this->index();
 	}
 
 
 	#[Get(path: "todos/loadList/{uniqid}", name: 'todos.loadList')]
 	public function loadList($uniqid) {
-		if (CacheManager::$cache->exists(self::KEY . $uniqid)) {
-			$list = CacheManager::$cache->fetch(self::KEY . $uniqid);
-			USession::set('list', $list);
-			$this->setInfo($uniqid);
+		if ($list=$this->service->persistentGet($uniqid)){
 			$this->showMessage('Chargement', "Liste chargée depuis <b>$uniqid</b>", 'success', 'check square outline');
 			$this->displayList($list);
 		} else {
@@ -187,7 +128,7 @@ class TodosController extends ControllerBase {
 		$this->index();
 	}
 
-	#[Route('{url}', priority: -1000)]
+	#[Route('{url}', requirements: ['url'=>"(?!admin)|(?!Admin).*?"], priority: -1000)]
 	public function p404($url){
 		echo "<div class='ui error inverted message'><div class='header'>404</div>The page `$url` you are looking for doesn't exist!</div>";
 	}
---
+++ app/services/SessionStorageList.php
@@ -0,0 +1,165 @@
+<?php
+namespace services;
+
+use Ubiquity\cache\CacheManager;
+use Ubiquity\controllers\Router;
+use Ubiquity\exceptions\CacheException;
+use Ubiquity\utils\http\USession;
+
+
+class SessionStorageList{
+	const CACHE_KEY = 'datas/lists/';
+	const EMPTY_LIST_ID='not saved';
+	const LIST_SESSION_KEY='list';
+	const ACTIVE_LIST_SESSION_KEY='active-list';
+
+	private array $info;
+
+	private array $message;
+
+	public function __construct($controller){
+		$this->jquery=$controller->jquery;
+		$this->info=['id'=>self::EMPTY_LIST_ID,'updated'=>false];
+		$this->message=[];
+	}
+
+	private function createCopyButton(string $bt,string $elmText){
+		$this->jquery->click($bt,'
+									let $temp = $("<input>");
+									$("body").append($temp);
+									$temp.val($("'.$elmText.'").text()).select();
+									document.execCommand("copy");
+									$temp.remove();
+									$("'.$bt.'").popup({content: "Copié!"}).popup("show");
+							');
+	}
+
+	private function getButtonUrl($url){
+		$this->createCopyButton('#btCopy','#url');
+		return '<span id="url" class="ui inverted label">'.$url.'</span>&nbsp;<div class="ui mini circular inverted basic icon button" id="btCopy"><i class="copy icon"></i></div>';
+	}
+
+	public function getList($initialize=true): array {
+		return USession::get(self::LIST_SESSION_KEY, $initialize?[]:null);
+	}
+
+	public function setList(array $list): void {
+		$this->listUpdated(true);
+		USession::set(self::LIST_SESSION_KEY, $list);;
+	}
+
+	public function listExist():bool{
+		return USession::exists(self::LIST_SESSION_KEY);
+	}
+
+	public function createList():array{
+		$list = USession::set(self::LIST_SESSION_KEY, []);
+		$this->setInfo(self::EMPTY_LIST_ID);
+		return $list;
+	}
+
+	public function listUpdated(bool $updated=false):void{
+		$info=USession::get(self::ACTIVE_LIST_SESSION_KEY,['id'=>self::EMPTY_LIST_ID]);
+		$this->setInfo($info['id'],$updated);
+	}
+
+	public function setInfo(string $id=null,bool $updated=false){
+		$this->info=['id'=>$id??self::EMPTY_LIST_ID,'updated'=>$updated];
+		if(isset($id)) {
+			USession::set(self::ACTIVE_LIST_SESSION_KEY, $this->info);
+		}
+	}
+
+	private function getUniqid(){
+		return ($this->info['id']===self::EMPTY_LIST_ID)?uniqid('',true):$this->info['id'];
+	}
+
+	private function getInfo():array{
+		return $this->info=USession::get(self::ACTIVE_LIST_SESSION_KEY,['id'=>self::EMPTY_LIST_ID]);
+	}
+
+	public function persistentGet(string $id):array|bool{
+		if (CacheManager::$cache->exists(self::CACHE_KEY . $id)) {
+			$list = CacheManager::$cache->fetch(self::CACHE_KEY . $id);
+			USession::set(self::LIST_SESSION_KEY, $list);
+			$this->setInfo($id);
+			return $list;
+		}
+		return false;
+	}
+
+	public function persistentExists(string $id):bool{
+		return CacheManager::$cache->exists(self::CACHE_KEY . $id);
+	}
+
+	public function persistentSave():string{
+		$this->getInfo();
+		$id = $this->getUniqid();
+		$this->setInfo($id);
+		try {
+			CacheManager::$cache->store(self::CACHE_KEY . $id, $this->getList());
+			$this->message['success']="La liste a été sauvegardée sous l'id <b>$id</b>.<br>Elle sera accessible depuis l'url " . $this->getButtonUrl(Router::url('todos.loadList', [$id]));
+		}catch(CacheException $e){
+			$this->message['error']=$e->getMessage();
+		}
+		return $id;
+	}
+
+	public function getTitle():string{
+		$this->getInfo();
+		$title=$this->info['id'];
+		if($this->info['updated']){
+			$title.=' [*]';
+		}
+		return $title;
+	}
+
+	public function addElements(string|array $elms):array{
+		$list = $this->getList();
+		if (\is_array($elms)) {
+			foreach ($elms as $e) {
+				if($e!='') {
+					$list[] = $e;
+				}
+			}
+		}elseif ($elms){
+			$list[] = $elms;
+		}
+		$this->setList($list);
+		return $list;
+	}
+
+	public function deleteElement(int $index):array{
+		$list = $this->getList();
+		if (isset($list[$index])) {
+			$toDelete=$list[$index];
+			unset($list[$index]);
+			$list=\array_values($list);
+			$this->setList($list);
+			$this->message['success']="L'élément <b>$toDelete</b> a été supprimé.";
+			return $list;
+		}
+		$this->message['error']="L'élément d'index <b>$index</b> n'existe pas.";
+		return $list;
+	}
+
+	public function updateElement(int $index,string $value):array{
+		$list = $this->getList();
+		if ($value && isset($list[$index])) {
+				$list[$index] = $value;
+				$this->setList($list);
+			$this->message['success']="L'élément à l'index <b>$index</b> a été modifié.";
+			return $list;
+		}
+		$this->message['error']="L'élément d'index <b>$index</b> n'existe pas.";
+		return $list;
+	}
+
+	public function getMessage(string $type):string {
+		return $this->message[$type]??'';
+	}
+
+	public function hasError():string {
+		return isset($this->message['error']);
+	}
+}
